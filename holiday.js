374c76958a2d2ca312e812bc4b763ce1c1a0608a
Sinha899 <143953851+Sinha899@users.noreply.github.com>
2024-01-03 16:23:30 -0600

    Add files via upload

---
+++ holiday.js
@@ -0,0 +1,10 @@
+function validate(){
+    a=document.getElementById("p1").value
+       b=document.getElementById("p2").value
+       if(a===b){
+        alert("Password reentered sucussfully")
+       }
+       else{
+        alert("Password reenterd unsucussfully")
+       }
+}
